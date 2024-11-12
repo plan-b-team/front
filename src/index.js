b42a8e779e9406a5af22454943145588e1829593
Hyun-EG <codiee@naver.com>
2024-11-12 18:36:09 +0900

    Fix: 경로수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/Modal";
+import App from "./App";
 import "./styles/main.scss";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
