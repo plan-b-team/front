1f7891792714506818c3fe4405f7be11fad9b86f
Song Jae Won <jwsonghub@gmail.com>
2024-11-13 00:22:35 +0900

    Design: navbar 디자인

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navWrapper">
+      <div className="navWrapper__planLogo">PLAN</div>
+      <div className="navWrapper__bLogo">B</div>
+      <div className="navWrapper__subtitleWrapper">
+        <div className="navWrapper__subtitleWrapper__title">POST</div>
+        <div className="navWrapper__subtitleWrapper__title">개발예정</div>
+        <div className="navWrapper__subtitleWrapper__title">개발예정</div>
+        <div className="navWrapper__subtitleWrapper__title">개발예정</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
